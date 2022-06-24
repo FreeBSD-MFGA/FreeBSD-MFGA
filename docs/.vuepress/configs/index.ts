9754fd95992c7d4ec3dadc38c37947b3d3be01f0
ykla <yklaxds@gmail.com>
2022-06-24 21:58:17 +0800

    delete useful

--- docs/.vuepress/configs/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @file configs/index.ts
- * @brief Configuration details.
- * @copyright Copyright (c) 2022 FreeBSD Chinese Community. All rights reserved.
- */
-
-export {sideBarConfig} from './sidebar';
